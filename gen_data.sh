0d2a4025a605919c90f8af7fb8d023ac329a4dd1
Ben Doan <ben@bendoan.me>
2014-09-02 17:06:50 -0500

    made gen_data more efficient

--- gen_data.sh
+++ gen_data.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/bash
 
+TMP_LOC="/tmp/im.jpg"
+
 source env/bin/activate
 
 while :;do
-    DATE=$(date +"%Y-%m-%d_%H%M%S")
-    raspistill -n -t 1 -o $HOME/data/im.jpg
-    sh crop.sh $HOME/data/im.jpg > /dev/null
+    raspistill -n -t 1 -o - | convert -verbose -crop 330x130+1500+850 - $TMP_LOC
 
-    CLASS=$(python classify $HOME/data/im.jpg)
+    DATE=$(date +"%Y-%m-%d_%H%M%S")
+    CLASS=$(python classify $TMP_LOC)
     echo $CLASS
-    if [ $CLASS == "noshuttle" ]; then
-        rm $HOME/data/im.jpg
-    else
-        mv $HOME/data/im.jpg $HOME/data/negatives/$CLASS$DATE.jpg
-        echo "Writing img"
-    fi
+    mv $TMP_LOC $HOME/data/negatives/$CLASS$DATE.jpg
 done
